ef8dd9ab8d1eabbd1e25df92fa5880ef96b7a4e4
Pattisier <Principursa@protonmail.com>
2026-02-03 12:49:33 -0500

    removed contracts and remappings

--- apps/protocol/src/strategies/lockups/interfaces/ILockupCCAFactory.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
-
-import {IDistributionStrategy} from "ll/interfaces/IDistributionStrategy.sol";
-
-/// @notice Interface for the lockup CCA factory
-interface ILockupCCAFactory is IDistributionStrategy {}
