dc0b48b6bc57b2160d6469f05de62dfd226eed30
1a35e1 <1a35e1@protonmail.com>
2026-02-03 17:42:16 +0000

    chore: Drop CCA, FHE deps

--- apps/protocol/lib/hardhat/console.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.9.0;
-
-// Minimal stub for Hardhat's console.sol to satisfy imports in tests.
-library console {
-    function log(string memory) internal pure {}
-}
