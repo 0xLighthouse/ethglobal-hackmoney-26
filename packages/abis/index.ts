d39545d6d1351205a22c5fab2857487f25723605
1a35e1 <1a35e1@protonmail.com>
2026-02-04 21:45:18 +0000

    feat: Gen ABIs from build

---
+++ packages/abis/index.ts
@@ -0,0 +1,5 @@
+import { erc20RefundableTokenSaleFactoryAbi } from './refundable-token-sale-factory'
+import { erc20RefundableTokenSaleAbi } from './refundable-token-sale'
+
+export const ERC20RefundableTokenSaleFactoryABI = erc20RefundableTokenSaleFactoryAbi
+export const ERC20RefundableTokenSaleABI = erc20RefundableTokenSaleAbi
