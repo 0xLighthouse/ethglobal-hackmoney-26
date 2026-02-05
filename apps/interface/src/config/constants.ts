76c2f51423bcae10b05a4235d81885e6ef4ff0d3
1a35e1 <1a35e1@protonmail.com>
2026-02-05 14:07:39 +0000

    feat: Create Sale Drawer

--- apps/interface/src/config/constants.ts
+++ apps/interface/src/config/constants.ts
@@ -6,4 +6,4 @@ export const BASE_SEPOLIA_FACTORY_ADDRESS =
 // TODO: For now just use USDC on Base Sepolia
 export const BASE_SEPOLIA_FUNDING_TOKEN_ADDRESS =
   (process.env.NEXT_PUBLIC_FUNDING_TOKEN_ADDRESS as `0x${string}` | undefined) ??
-  "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
+  "0x036CbD53842c5426634e7929541eC2318f3dCF7e";
