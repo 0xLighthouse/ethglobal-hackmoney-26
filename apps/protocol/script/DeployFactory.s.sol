e2aa627e2db4d38c9355c0ae418fea4491381631
1a35e1 <1a35e1@protonmail.com>
2026-02-06 17:45:18 +0000

    chore: WIP

--- apps/protocol/script/DeployFactory.s.sol
+++ apps/protocol/script/DeployFactory.s.sol
@@ -7,6 +7,12 @@ import {ERC20RefundableTokenSaleFactory} from "../src/ERC20RefundableTokenSaleFa
 /// @notice Deploys the ERC20RefundableTokenSaleFactory contract
 /// @dev Run with: forge script script/DeployFactory.s.sol:DeployFactory --rpc-url <RPC_URL> --broadcast --verify
 contract DeployFactory is Script {
+
+    // Base Sepolia
+    address public sepoliaPoolManager = 0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408;
+    address public sepoliaPositionManager = 0x4B2C77d209D3405F41a037Ec6c77F7F5b8e2ca80;
+    address public sepoliaPermit2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+
     function run() external returns (ERC20RefundableTokenSaleFactory factory) {
         // Get deployer from private key
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
@@ -18,9 +24,9 @@ contract DeployFactory is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         // Get pool manager address from environment
-        address poolManager = vm.envAddress("POOL_MANAGER");
-        address positionManager = vm.envAddress("POSITION_MANAGER");
-        address permit2 = vm.envAddress("PERMIT2");
+        address poolManager = sepoliaPoolManager;
+        address positionManager = sepoliaPositionManager;
+        address permit2 = sepoliaPermit2;
 
         // Deploy the factory
         factory = new ERC20RefundableTokenSaleFactory(poolManager, positionManager, permit2);
